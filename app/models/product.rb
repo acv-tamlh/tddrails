fc0bea5a0fb27361b151e99604b1924d29b359e9
TamLH <tamlh@atmarkcafe.org>
2018-03-20 16:08:10 +0700

    rspec strip html product.description

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
   validates :title, :description, :price, presence: true
   validates_numericality_of :price, greater_than: 0
-
+  before_save :strip_html_description
   belongs_to :category
+
+  def strip_html_description
+    self.description = ActionView::Base.full_sanitizer.sanitize(self.description)
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Product, type: :model do
   it {should validate_presence_of(:price)}
   it {should validate_numericality_of(:price).is_greater_than(0)}
   it {should belong_to(:category)}
+
+  context "Strip html from description" do
+    it "validate the strip html description" do
+      product = Product.new(title: 'test', description: '<p>this is description</p>', price: 12)
+      product.save
+      expect(product.description).to eq 'this is description'
+    end
+  end
 end
