b406d67fc95facee35e9ed67b9e4c17793347824
TamLH <tamlh@atmarkcafe.org>
2018-03-21 10:26:49 +0700

    add rspec test model product

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180321032340) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title"
+    t.string "description"
+    t.decimal "price", precision: 10
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
 
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  it {should validate_presence_of(:title)}
+  it {should validate_presence_of(:description)}
+  it {should validate_presence_of(:price)}
+  it {should validate_numericality_of(:price).is_greater_than(0)}
+  it {should belong_to(:category)}
+
+  context "Strip html from description" do
+    it "validate the strip html description" do
+      Category.create(title: 'title category')
+      product = Product.new(title: 'test', description: '<p>this is description</p>', price: 12, category_id: Category.last.id)
+      product.save
+      expect(product.description).to eq 'this is description'
+    end
+    it "validate lowercase title product" do
+      Category.create(title: 'title category')
+      product = Product.new(title: 'TEST', description: 'this is description', price: 12, category_id: Category.last.id)
+      product.save
+      # byebug
+      expect(product.title).to eq 'test'
+    end
+
+    it "validate title shotter than description" do
+      Category.create(title: 'new')
+      product = Product.new(title: 'test asd ad asd asd asd ', description: 'this is description', price: 12, category_id: Category.last.id)
+      product.save
+      # byebug
+      expect(product.errors.messages).to include(description: ["cant be shorter than title"])
+    end
+  end
+end
