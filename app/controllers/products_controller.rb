e83bde6127d02638d385318de828643113edc57a
TamLH <tamlh@atmarkcafe.org>
2018-03-21 11:45:56 +0700

    rspec test controller get all product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,21 @@
 class ProductsController < ApplicationController
+  before_action :product_params, only:[:create]
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    return redirect_to products_path if @product.save
+    render :new
+  end
+
+  private
+    def product_params
+      product_params = params.require(:product).permit(:title, :description, :price, :category_id)
+    end
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe ProductsController, type: :controller do
   describe '#index' do
     it "get list of product" do
@@ -12,4 +11,23 @@ RSpec.describe ProductsController, type: :controller do
       expect(assigns(:products)).to eq products
     end
   end
-end
+  # describe "#new" do
+  #   before do
+  #     FactoryBot.create(:product)
+  #   end
+  #   it "permit param" do
+  #     Category.create(title: '1')
+  #     params = {
+  #       product: {
+  #         title: '123',
+  #         description: '1234',
+  #         price: 12,
+  #         category_id: 1
+  #       }
+  #     }
+  #     # byebug
+  #     should permit(:title, :description, :price, :category_id).
+  #           for(:create, params: params).on(:product)
+  #           end
+  #       end
+    end
